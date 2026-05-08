bbdd5e3cce270c372eba4f484465d4dd32f6c6e9
carminenicotera <carmine.91.n@hotmail.it>
2026-05-08 10:14:29 +0200

    add footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,7 +2,13 @@ export default function AppFooter() {
 
   return (
     <>
-      <footer>Footer</footer>
+      <footer className="bg-light text-center py-3 mt-4">
+        <div className="container">
+          <p className="mb-0">
+            &copy; { new Date().getFullYear() } BoolComics. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </>
   )
 }
