35522116bcc0fcc3ca1aeb56b4de6a082ee336e0
carminenicotera <carmine.91.n@hotmail.it>
2026-05-09 11:35:05 +0200

    Add responsive catalog cards with Bootstrap layout

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function CatalogPage() {
 
@@ -14,48 +15,63 @@ export default function CatalogPage() {
 
   return (
     <>
-      <div className="container">
-        <h1 className="my-5">Catalogo dei Fumetti</h1>
-        <p>Qui potrai esplorare il nostro vasto catalogo di fumetti e manga. Scopri le ultime uscite e i classici intramontabili!</p>
-      </div>
+      {/* PAGE HEADER */ }
+      <section className="py-5 bg-light">
+        <div className="container">
+          <h1 className="fw-bold text-uppercase mb-3">
+            Catalogo
+          </h1>
+          <p className="text-secondary">
+            Qui potrai esplorare il nostro vasto catalogo
+            di fumetti e manga.
+          </p>
+        </div>
+      </section>
 
-      <div className="container py-5">
+      {/* PRODUCTS */ }
+      <section className="py-5">
+        <div className="container">
+          <div className="row g-4">
+            { comics.map(comic => (
+              <div key={ comic.id } className="col-12 col-sm-6 col-lg-3">
+                <div className="card h-100 border-0 shadow-sm">
 
-        <h1 className="mb-4">
-          Catalogo
-        </h1>
+                  {/* IMAGE */ }
+                  <Link to={ `/comics/${comic.id}` }>
+                    <img src="/public/img/placeholdercomic.png" alt={ comic.name } className="card-img-top object-fit-cover" />
+                  </Link>
 
-        <div className="row g-4">
+                  {/* BODY */ }
+                  <div className="card-body text-center d-flex flex-column">
 
-          { comics.map(comic => (
+                    {/* TITLE */ }
+                    <h5 className="fw-bold">
+                      { comic.name }
+                    </h5>
 
-            <div key={ comic.id } className="col-12 col-sm-6 col-lg-3">
+                    {/* PRICE */ }
+                    <p className="fs-5 fw-bold mt-2">
+                      € { comic.price }
+                    </p>
 
-              <div className="card h-100">
+                    {/* BUTTON */ }
+                    <button className="btn btn-danger mt-auto text-white fw-bold text-uppercase">
+                      Acquista
+                    </button>
 
-                <img src={ comic.image_url } className="card-img-top" alt={ comic.name } />
-
-                <div className="card-body">
-
-                  <h5>
-                    { comic.name }
-                  </h5>
-
-                  <p>
-                    € { comic.price }
-                  </p>
+                  </div>
 
                 </div>
 
               </div>
 
-            </div>
+            )) }
 
-          )) }
+          </div>
 
         </div>
 
-      </div>
+      </section>
     </>
   )
 }
