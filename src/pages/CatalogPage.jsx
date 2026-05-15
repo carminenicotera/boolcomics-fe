f8a441f52e391bc860e4788e64bc9e988236398f
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-05-15 13:50:56 +0200

    add logic for the filter

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -55,11 +55,11 @@ export default function CatalogPage({ addToCart }) {
 
           {/* TOP BAR */}
           {searchQuery && (
-            <div className="row justify-content-between align-items-center mb-4 g-3">
+            <div className={`row justify-content-${filteredComics.length === 0 ? 'center' : 'between'} align-items-center mb-4 g-3`}>
 
               {/* RESULTS */}
               <div className="col-12 col-md-auto">
-                {filteredComics.length === 0 && (
+                {filteredComics.length === 0 ? (
 
                   <div class="search-empty-state">
                     <div class="empty-badge">OPS!</div>
@@ -68,38 +68,38 @@ export default function CatalogPage({ addToCart }) {
 
                     <p class="empty-message">
                       La ricerca per  non ha portato alla luce nessun volume.
-                      
+
                       Prova a digitare una nuova parola chiave.
                     </p>
 
                     <Link to="/catalog" class="btn btn-outline-danger">Mostra tutto il catalogo</Link>
                   </div>
 
-                )}
-                {filteredComics.length !== 0 && (
+                ) : (
 
                   <p className="results-text mb-0">
                     {filteredComics.length} prodotti trovati
                   </p>
-
                 )}
+                
               </div>
-
-
-
 
 
               {/* SORT BY */}
-              
-              <div className="col-12 col-md-3">
-                <select className="form-select catalog-select" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-                  <option value="">Ordina per</option>
-                  <option value="name">Nome</option>
-                  <option value="low-price">Prezzo: dal più basso</option>
-                  <option value="high-price">Prezzo: dal più alto</option>
-                  <option value="recent">Più recenti</option>
-                </select>
-              </div>
+              {filteredComics.length !== 0 && (
+
+                <div className="col-12 col-md-3">
+                  <select className="form-select catalog-select" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                    <option value="">Ordina per</option>
+                    <option value="name">Nome</option>
+                    <option value="low-price">Prezzo: dal più basso</option>
+                    <option value="high-price">Prezzo: dal più alto</option>
+                    <option value="recent">Più recenti</option>
+                  </select>
+                </div>
+
+
+              )}
             </div>
           )}
 
